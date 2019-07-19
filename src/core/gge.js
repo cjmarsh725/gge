b1ce562ebd7cf04468303e9a35740501960b3575
Chris <cjmarsh725@gmail.com>
2019-07-19 13:35:36 -0700

    Testing shaders, program, locations, and buffers

--- src/core/gge.js
+++ src/core/gge.js
@@ -1,7 +1,9 @@
 export { setup } from "./setup";
-export { renderer } from '../renderer';
-import { sceneManager } from '../scene';
 
+import { renderer } from '../renderer';
+export const run = renderer.run;
+
+import { sceneManager } from '../scene';
 Object.defineProperty(exports, 'scene', { 
   get() { return sceneManager.loadScene() },
   set(val) { console.error("Scene cannot be modified directly, try scene.load instead") }
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,10 +1,55 @@
+import { ggi } from '../core/internal';
+import { vertShaders, fragShaders } from './shaders';
+import { loadShader, createProgram } from './utils';
 
-const _name = new WeakMap();
+// const _name = new WeakMap();
 
 export default class Renderer {
   constructor() {
-    _name.set(this, "foo");
+    // _name.set(this, "foo");
   }
   
-  
+  testing() {
+    const gl = ggi.gl;
+    const program = createProgram(gl, [
+      loadShader(gl, vertShaders.drawImage, gl.VERTEX_SHADER),
+      loadShader(gl, fragShaders.drawImage, gl.FRAGMENT_SHADER)
+    ]);
+    // locations for vertex data
+    const posLoc = gl.getAttribLocation(program, "a_position");
+    const texcoordLoc = gl.getAttribLocation(program, "a_texcoord");
+    // lookup uniforms
+    const matrixLoc = gl.getUniformLocation(program, "u_matrix");
+    const texLoc = gl.getUniformLocation(program, "u_texture");
+    // create a buffer for the unit quad
+    const posBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
+    const positions = [
+      0, 0,
+      0, 1,
+      1, 0,
+      1, 0,
+      0, 1,
+      1, 1,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    // create a buffer for texture coords
+    const texcoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+    const texcoords = [
+      0, 0,
+      0, 1,
+      1, 0,
+      1, 0,
+      0, 1,
+      1, 1,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texcoords), gl.STATIC_DRAW);
+
+
+  }
+
+  run() {
+    this.testing();
+  }  
 }
