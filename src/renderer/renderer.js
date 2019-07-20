f4abcf7f9cd7bb02173017c7182d21dbc7fe3001
Chris <cjmarsh725@gmail.com>
2019-07-20 10:06:20 -0700

    Added test image, and loading of texture

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -9,7 +9,7 @@ export default class Renderer {
     // _name.set(this, "foo");
   }
   
-  testing() {
+  draw() {
     const gl = ggi.gl;
     const program = createProgram(gl, [
       loadShader(gl, vertShaders.drawImage, gl.VERTEX_SHADER),
@@ -33,7 +33,7 @@ export default class Renderer {
       1, 1,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-    // create a buffer for texture coords
+    // create a buffer for the texture coords
     const texcoordBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
     const texcoords = [
@@ -46,10 +46,44 @@ export default class Renderer {
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texcoords), gl.STATIC_DRAW);
 
+  }
+  
+  createTextureInfo(url) {
+    const gl = ggi.gl;
+    // load image and get texture info
+    const tex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, tex);
+    // set placeholder 1x1 transparent image
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, 
+              gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 0, 0]));
+    // set parameters for non power of 2 images
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  
+    const textureInfo = {
+      width: 1,
+      height: 1,
+      texture: tex,
+    };
+    const img = new Image();
+    img.addEventListener('load', () => {
+      textureInfo.width = img.width;
+      textureInfo.height = img.height;
+      gl.bindTexture(gl.TEXTURE_2D, textureInfo.texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+    });
+    img.src = url;
 
+    return textureInfo;
+  }
+
+  render(time) {
+    this.draw();
+    requestAnimationFrame(this.render);
   }
 
   run() {
-    this.testing();
+    requestAnimationFrame(this.render);
   }  
 }
